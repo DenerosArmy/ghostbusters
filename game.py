eeeb142fb2df52c2f8ccec0e3eb0addd4e29649b
Nikita Kitaev <nikitakit@gmail.com>
2012-11-29 20:20:35 -0800

    Initial message processing

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ from threading import Thread
 from Queue import Queue, Empty
 import time
 from utils import *
+import json
+import distribution
 
 class GameState(object):
     def __init__(self, width, height,
@@ -11,6 +13,9 @@ class GameState(object):
                  y_dir=(37.484911,-122.147929)):
         self.players = []
         self.probability_cloud = {}
+        self.ghost_cloud = {}
+        self.ghost_cloud["Ghost1"] = distribution.Distribution()
+        self.ghost_cloud["Ghost1"].initialize_randomly()
         self.receiving = False
         self.simp_to_geo = transform_mtx(width, height, origin, x_dir, y_dir)
         self.geo_to_simp = inverse(self.simp_to_geo)
@@ -21,7 +26,8 @@ class GameState(object):
 
     def add_player(self, name):
         self.players.append(name)
-        self.probability_cloud[name] = ProbabilityCloud()
+        self.probability_cloud[name] = distribution.Distribution()
+        self.probability_cloud[name].initialize_randomly()
 
     def push(self, timestamp, msg, callback):
         self.queue.put((timestamp, msg, callback))
@@ -31,5 +37,15 @@ class GameState(object):
             timestamp, msg, callback = self.queue.get()
             if time.time() - timestamp > 1.0:
                 continue # Ignore out-of-date data
+
+            contents = json.loads(msg)
+            dist = self.probability_cloud.values()[0]
+            dist.update(contents["args"])
             print "Received message", msg
-            callback("")
+
+            if contents["action"] == "compass":
+                args = [0.25, 0.25, 0.25, 0.25]
+            else:
+                args = content["args"][0], content["args"][1]
+            res = {"action": contents["action"], "args": args}
+            callback(json.dumps(res))
