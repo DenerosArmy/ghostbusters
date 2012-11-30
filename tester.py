01203ca6a7af0a5fb5c1677a1c55e964c1ee78e5
Nikita Kitaev <nikitakit@gmail.com>
2012-11-29 22:49:57 -0800

    Fix tester

--- tester.py
+++ tester.py
@@ -13,7 +13,6 @@ def compass(x=0.0, y=0.0, acc=0.0, heading=0.0):
     ws.send(json.dumps(data))
 
 def ghost(x=0.0, y=0.0, acc=0.0, heading=0.0):
-    ws = create_connection("ws://0.0.0.0:9000/data")
     data = {"action": "compass", "args": [x, y, acc, heading]}
     ws.send(json.dumps(data))
 
