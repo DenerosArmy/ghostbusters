763b51dbd9dcbf84371714bd2f8dcca711dbee57
Nikita Kitaev <nikitakit@gmail.com>
2012-11-29 19:37:10 -0800

    Multithreading/queue code

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 from math import *
-
+from threading import Thread
+from Queue import Queue, Empty
+import time
 
 class GameState(object):
     def __init__(self, width, height,
@@ -11,10 +13,22 @@ class GameState(object):
         self.receiving = False
         self.simp_to_geo = transform_mtx(width, height, origin, x_dir, y_dir)
         self.geo_to_simp = inverse(self.simp_to_geo)
+        self.queue = Queue()
+        self.thread = Thread(target=self.run_thread)
+        self.thread.daemon = True # thread dies with the program
+        self.thread.start()
 
     def add_player(self, name):
         self.players.append(name)
         self.probability_cloud[name] = ProbabilityCloud()
 
     def push(self, timestamp, msg, callback):
-        pass
+        self.queue.put((timestamp, msg, callback))
+
+    def run_thread(self):
+        while True:
+            timestamp, msg, callback = self.queue.get(timeout=.1)
+            if time.time() - timestamp > 1.0:
+                continue # Ignore out-of-date data
+            print "Received message", msg
+            callback()
